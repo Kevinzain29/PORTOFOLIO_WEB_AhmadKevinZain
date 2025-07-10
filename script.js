1d75e5e860db8816d1776b50c79e0a3831afefa2
Kevinzain29 <ahmad.kevin.ft22@mail.umy.ac.id>
2025-07-10 21:29:17 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function showEmail() {
+    const email = document.getElementById('email');
+    email.style.display = 'block';
+}
+AOS.init({
+    duration: 2000, // durasi animasi (ms)
+    // once: true      // animasi hanya muncul sekali
+});
+// Event listener untuk semua gambar di dalam .card
+document.querySelectorAll('.card img').forEach(img => {
+    img.addEventListener('click', function() {
+        document.getElementById('modalImage').src = this.src;
+        document.getElementById('imgModal').style.display = 'flex';
+    });
+});
+
+// Fungsi untuk menutup modal
+function closeModal() {
+    document.getElementById('imgModal').style.display = 'none';
+}
+
+
+
